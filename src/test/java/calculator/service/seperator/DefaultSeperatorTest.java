ad7178124d0aa92e475fbc849c4aa35f7d624ef9
K-KY <kyuyoungk@naver.com>
2025-10-16 22:36:53 +0900

    test : 기본 구분자 문자열 분리 테스트
    - 분리 후 반환 타입
    - 리스트의 제네릭 객체 타입
    - 분리 후 길이
    - 분리한 문자열

---
+++ src/test/java/calculator/service/seperator/DefaultSeperatorTest.java
@@ -0,0 +1,40 @@
+package calculator.service.seperator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DefaultSeperatorTest {
+
+    @Test
+    @DisplayName("분리한 반환값은 리스트 타입")
+    void seperatorTest() {
+        Separator seperator = new DefaultSeperator();
+        assertThat(seperator.separate("123:123")).isInstanceOf(List.class);
+    }
+
+    @Test
+    @DisplayName("리스트의 값은 String")
+    void seperatorReturnsStringTest() {
+        Separator seperator = new DefaultSeperator();
+        seperator.separate("123:123")
+                .forEach(s -> assertThat(s).isInstanceOf(String.class));
+    }
+
+    @Test
+    @DisplayName("기본 구분자 문자열 분리 테스트")
+    void seperatorLiteralTest() {
+        Separator seperator = new DefaultSeperator();
+        assertThat(seperator.separate("123:213").size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("문자열 반환 테스트")
+    void seperatorLiteralReturnsValidTest() {
+        Separator seperator = new DefaultSeperator();
+        assertThat(seperator.separate("123").getFirst()).isEqualTo("123");
+    }
+}
