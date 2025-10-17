c1a7d6502625b0a2d669126d9f0b89894760a814
K-KY <kyuyoungk@naver.com>
2025-10-17 19:47:29 +0900

    refactor : 기본 분리 클래스 싱글톤 패턴 적용
    - 기본 분리 클래스는 동일한 구분자를 사용하므로 바뀌지 않음 따라서 싱글톤 적용

--- src/main/java/calculator/service/seperator/DefaultSeperator.java
+++ src/main/java/calculator/service/seperator/DefaultSeperator.java
@@ -8,6 +8,11 @@ public class DefaultSeperator implements Separator {
 
     private static final List<Delimiter> delimiter = List.of(Delimiter.values());
     private static final String SPACE = " ";
+    private static final DefaultSeperator instance = new DefaultSeperator();
+
+    public static DefaultSeperator getInstance() {
+        return instance;
+    }
 
     @Override
     public List<String> separate(String target) {
