27aadc6a6ffe566860ec25fc4ea3d297ab7d4f7d
K-KY <kyuyoungk@naver.com>
2025-10-19 01:58:27 +0900

    feat : Expression에 메서드 추가
    - 구분자가 비어있는지 확인하는 메서드
    - 가장 첫 번째 구분자를 반환하는 메서드

--- src/main/java/calculator/service/dto/Expression.java
+++ src/main/java/calculator/service/dto/Expression.java
@@ -2,4 +2,12 @@ package calculator.service.dto;
 
 import java.util.List;
 
-public record Expression(String expression, List<String> delimiter) {}
+public record Expression(String expression, List<String> delimiter) {
+    public boolean isDelimiterEmpty() {
+        return delimiter.isEmpty();
+    }
+
+    public String getFirstDelimiter() {
+        return delimiter.getFirst();
+    }
+}
