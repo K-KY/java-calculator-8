449ce2752ec212cafff0f8e9f038191a923d5d21
K-KY <kyuyoungk@naver.com>
2025-10-19 02:54:02 +0900

    feat : 더하기 요청을 받을 컨트롤러

--- src/main/java/calculator/controller/AddCalculatorController.java
+++ src/main/java/calculator/controller/AddCalculatorController.java
@@ -1,18 +1,26 @@
 package calculator.controller;
 
+import calculator.service.Calculator;
+import calculator.service.dto.Expression;
+import calculator.service.seperator.Deserializer;
+
 public class AddCalculatorController {
 
-    public static AddCalculatorController instance = new AddCalculatorController();
-    private AddCalculatorController() {}
+    private static AddCalculatorController instance;
+    private final Calculator calculator;
+    private AddCalculatorController(Calculator calculator) {
+        this.calculator = calculator;
+    }
 
     public static AddCalculatorController getInstance() {
         if (instance == null) {
-            instance = new AddCalculatorController();
+            instance = new AddCalculatorController(new Calculator());
         }
         return instance;
     }
 
     public Integer add(String input) {
-        return 0;
+        Expression parse = Deserializer.parse(input.trim());
+        return calculator.add(parse);
     }
 }
