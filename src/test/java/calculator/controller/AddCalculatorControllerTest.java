54a7d91508c65926f99482ba8c9075cee17db4a1
K-KY <kyuyoungk@naver.com>
2025-10-19 02:54:50 +0900

    test : AddCalculatorController 테스트
    - 이미 다른 테스트에서 검증 했으므로 단순 호출 테스트만 수행

--- src/test/java/calculator/controller/AddCalculatorControllerTest.java
+++ src/test/java/calculator/controller/AddCalculatorControllerTest.java
@@ -1,24 +1,26 @@
 package calculator.controller;
 
-import calculator.Application;
-import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class AddCalculatorControllerTest extends NsTest {
+class AddCalculatorControllerTest {
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
 
     private final AddCalculatorController controller = AddCalculatorController.getInstance();
 
     @Test
-    public void addTest() {
-        Integer add = controller.add("test");
+    @DisplayName("기본 구분자 더하기 테스트")
+    void defaultAddTest() {
+        Integer add = controller.add("123:123,123");
+        assertThat(add).isEqualTo(369);
+    }
 
-        assertThat(add).isEqualTo(0);
+    @Test
+    @DisplayName("사용자 정의 구분자 더하기 테스트")
+    void customAddTest() {
+        Integer add = controller.add("//;\\n123;123;123");
+        assertThat(add).isEqualTo(369);
     }
 }
