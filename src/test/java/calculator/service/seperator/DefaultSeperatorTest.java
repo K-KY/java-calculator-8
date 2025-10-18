143063c1c0989a188b65e1d185ae48e073c198c2
K-KY <kyuyoungk@naver.com>
2025-10-19 00:28:45 +0900

    test : Separator 테스트에 인수 변경 적용

--- src/test/java/calculator/service/seperator/DefaultSeperatorTest.java
+++ src/test/java/calculator/service/seperator/DefaultSeperatorTest.java
@@ -1,5 +1,6 @@
 package calculator.service.seperator;
 
+import calculator.service.dto.Expression;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,18 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultSeperatorTest {
 
+    private final Expression expression = new Expression("123:123", List.of());
     @Test
     @DisplayName("분리한 반환값은 리스트 타입")
     void seperatorTest() {
         Separator seperator = DefaultSeperator.getInstance();
-        assertThat(seperator.separate("123:123")).isInstanceOf(List.class);
+        assertThat(seperator.separate(expression)).isInstanceOf(List.class);
     }
 
     @Test
     @DisplayName("리스트의 값은 String")
     void seperatorReturnsStringTest() {
         Separator seperator = DefaultSeperator.getInstance();
-        seperator.separate("123:123")
+        seperator.separate(expression)
                 .forEach(s -> assertThat(s).isInstanceOf(String.class));
     }
 
@@ -28,13 +30,13 @@ class DefaultSeperatorTest {
     @DisplayName("기본 구분자 문자열 분리 테스트")
     void seperatorLiteralTest() {
         Separator seperator = DefaultSeperator.getInstance();
-        assertThat(seperator.separate("123:213").size()).isEqualTo(2);
+        assertThat(seperator.separate(expression).size()).isEqualTo(2);
     }
 
     @Test
     @DisplayName("문자열 반환 테스트")
     void seperatorLiteralReturnsValidTest() {
         Separator seperator = DefaultSeperator.getInstance();
-        assertThat(seperator.separate("123").getFirst()).isEqualTo("123");
+        assertThat(seperator.separate(expression).getFirst()).isEqualTo("123");
     }
 }
