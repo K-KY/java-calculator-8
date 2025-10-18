f6114c738da0490bb64c0928ab32a1d476a97ad4
K-KY <kyuyoungk@naver.com>
2025-10-19 01:19:12 +0900

    test : INPUT_RESULT 문자열 변경 적용

--- src/test/java/calculator/view/output/MessagesTest.java
+++ src/test/java/calculator/view/output/MessagesTest.java
@@ -10,7 +10,7 @@ class MessagesTest {
     @Test
     @DisplayName("상수 반환 테스트")
     void messagesTest() {
-        assertThat(Messages.INPUT_RESULT.getMessage()).isEqualTo("결과");
+        assertThat(Messages.INPUT_RESULT.getMessage()).isEqualTo("결과 : ");
         assertThat(Messages.INPUT_NUMBERS.getMessage()).isEqualTo("덧셈할 문자열을 입력해 주세요.");
         assertThat(Messages.ERROR_OCCURRED.getMessage()).isEqualTo("오류가 발생 했습니다.");
     }
